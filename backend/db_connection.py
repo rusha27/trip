d6618e1ffc88f33bf5091bc775d26ef33ac9c7a6
Rusha Shah <rushashah.126@gmail.com>
2025-06-18 12:03:36 +0530

    updated db_connection.py with env variables

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -1,17 +1,20 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 import logging
 
 # Setup logger (basic config, you can customize)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+load_dotenv()
 
 def get_db_connection():
     try:
         db = mysql.connector.connect(
-            host="13.234.30.109",
-            user="root",
-            password="rootmysql",
-            database="tripglide"
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME')
         )
         if db.is_connected():
             logger.info("Initial DB connection successful")
